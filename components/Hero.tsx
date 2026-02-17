77396a652e99d76ffcea3f04615fd3d9a8521bc9
Kelly Nkohla <kellynkohla0@gmail.com>
2026-02-17 17:00:47 +0200

    coderabbit suggestions

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="flex flex-col justify-center items-center text-center drop-shadow-sm p-20 min-h-screen bg-[url('/Hero.png')] bg-cover bg-center">
         <h1 className="text-5xl font-bold text-white">Welcome</h1>
         <p className="text-white text-lg font-semibold">Ready to manage your referees and tournaments/leagues?</p>
-        <Button><Link href="/tournaments">Let's Get Started</Link></Button>
+        <Button asChild><Link href="/tournaments">Let's Get Started</Link></Button>
     </section>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex sticky top-0 pl-8 bg-transparent rounded-4xl shadow-lg z-50  md:items-center md:sticky md:top-0 md:left-120 md:w-[430px] ">
-      <nav className="flex justify-center items-center font-semibold bordered gap-9 p-2 m-2 md:gap-5   ">
+    <div className="flex sticky top-0 pl-8 bg-transparent rounded-4xl shadow-lg z-50  md:items-center md:sticky md:top-0 md:left-[120px] md:w-[430px] ">
+      <nav className="flex justify-center items-center font-semibold gap-9 p-2 m-2 md:gap-5   ">
         <Link href="/">Home</Link>
         <Link href="/referees">Referees</Link>
         <Link href="/tournaments">Tournaments</Link>
